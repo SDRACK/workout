d9c52fb42effb9d3e6c6b8c5bdcef8ef85ca046e
Steve Rackham <steve.rackham@audioboom.com>
2018-02-26 16:31:26 +0000

    Switch to select method for workout generation following benchmarking

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -2,10 +2,12 @@ module Api
   module V1
     class WorkoutsController < ActionController::API
       def show
-        @exercises = Exercise::BODYPARTS.map do |bp|
-          Exercise.public_send(bp).sample
+        @exercises = Exercise.all
+
+        Exercise::BODYPARTS.map do |bp|
+          @exercises.select { |e| e[:bodypart] == bp }.sample
         end.shuffle
-        # render json: { exercises: @exercises }
+        
         render json: @exercises
       end
     end
